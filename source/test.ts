3b0c1bca68cc52918788984ca9126a1460d9a80f
Benjamin Lupton <b@lupton.cc>
2019-03-12 19:29:04 +1100

    v1.0.0 - changelog and version bump

---
+++ source/test.ts
@@ -0,0 +1,20 @@
+import { equal } from 'assert-helpers'
+import kava from 'kava'
+
+import memo from './'
+
+let count = 0
+function counter() {
+	return ++count
+}
+
+kava.suite('memo', function(suite, test) {
+	test('works as expected', function() {
+		const fn = memo(counter)
+		equal(count, 0)
+		equal(fn(), 1)
+		equal(count, 1)
+		equal(fn(), 1)
+		equal(count, 1)
+	})
+})
