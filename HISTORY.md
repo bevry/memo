4d8cb2b3f69e47769c63df7962119a96feb4bd77
Benjamin Lupton <b@lupton.cc>
2019-03-12 21:39:32 +1100

    v1.0.1 - better docs

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,9 @@
 # History
 
+## v1.0.1 2019 March 12
+
+-   Better documentation
+
 ## v1.0.0 2019 March 12
 
 -   Initial working version, extracted from [bevry/meet](https://github.com/bevry/meet), and published using [base files](https://github.com/bevry/base) and [editions](https://editions.bevry.me) via [boundation](https://github.com/bevry/boundation)
--- source/index.ts
+++ source/index.ts
@@ -1,3 +1,7 @@
+/**
+ * Wraps the callback in a memo that reuses the first invocation result of the callback to avoid subsequent invocations.
+ * @param callback The callback (that takes no arguments) that we wish to cache the result for.
+ */
 export default function memo<T>(callback: () => T): typeof callback {
 	let value: T
 	return () => (typeof value !== 'undefined' ? value : (value = callback()))
